bb2c176fa6caeb6a8d7bcdf856a87261191d6690
Flinn <358530466@qq.com>
2020-09-20 16:42:12 +0800

    开始编写chart页

--- src/views/Chart.tsx
+++ src/views/Chart.tsx
@@ -1,12 +1,10 @@
 import React from 'react'
 import Layout from 'components/Layout'
-import NoData from 'components/NoData'
 
 export default function () {
   return (
     <Layout>
       <div>
-        <NoData />
       </div>
     </Layout>
   )
